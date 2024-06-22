5d87ec267414bdc030d9c3f7d852125d3b2fa534
Sevi68 <sevindupunsara313@gmail.com>
2024-06-22 15:21:47 +0530

    create main component

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,13 +1,69 @@
-import React from 'react';
-import './main.css'
-
+import React from "react";
+import "./main.css";
+import img from "../../Assets/dubai.jpg";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { IoIosArrowForward } from "react-icons/io";
 const Main = () => {
+  const data = [
+    {
+      id: 1,
+      imgSrc: img,
+      destTitle: "Burj Khalifa",
+      location: "Dubai",
+      grade: "Relax",
+      fees: "$700",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, ducimus, aut tenetur dolorum aliquid eum natus ipsa harum porro inventore eius libero! Autem cupiditate commodi, nihil repudiandae facere architecto accusamus",
+    },
+  ];
 
-    return (
-        <div>
-         main   
-        </div>
-    );
+  return (
+    <section className="main container section">
+      <div className="secTitle">
+        <h3 className="title">Most visited destination</h3>
+      </div>
+
+      <div className="secContect grid">
+        {data.map(
+          ({ id, imgSrc, destTitle, location, grade, fees, description }) => {
+            return (
+              <div key={id} className="singleDestination">
+                <div className="imageDiv">
+                  <img src={imgSrc} alt={destTitle} />
+                </div>
+                <div className="cardInfo">
+                  <h4 className="destTitle">{destTitle}</h4>
+                  <span className="continent flex">
+                    <HiOutlineLocationMarker className="icon" />
+                    <span className="name">{location}</span>
+                  </span>
+
+                  <div className="fees flex">
+                    <div className="garde">
+                      <span>
+                        {grade}
+                        <small>+1</small>
+                      </span>
+                    </div>
+                    <div className="price">
+                     <h5>{fees}</h5>
+                    </div>
+                  </div>
+                  <div className="desc">
+                    <p>{description}</p>
+                  </div>
+                  <button className="btn flex">
+                    Details
+                  <IoIosArrowForward className="icon"/>
+                  </button>
+                </div>
+              </div>
+            );
+          }
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default Main;
